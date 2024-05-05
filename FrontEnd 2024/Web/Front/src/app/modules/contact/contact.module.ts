05fdb39bfd95acc01dee2442ce604f49209affad
Estefania Santana <estefania.msf10@gmail.com>
2024-05-05 18:27:37 -0300

    Se importan los modulos en el modulo de contacto

--- FrontEnd 2024/Web/Front/src/app/modules/contact/contact.module.ts
+++ FrontEnd 2024/Web/Front/src/app/modules/contact/contact.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ContactPageComponent } from 'src/app/pages/contact/contact-page/contact-page.component';
 import { ContactInfoComponent } from 'src/app/pages/contact/contact-info/contact-info.component';
+import { ContactFormComponent } from 'src/app/pages/contact/contact-form/contact-form.component';
 
 @NgModule({
-  declarations: [ContactPageComponent, ContactInfoComponent],
-  imports: [CommonModule],
-  exports: [ContactPageComponent, ContactInfoComponent]
+  declarations: [ContactPageComponent, ContactInfoComponent, ContactFormComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule,],
+  exports: [ContactPageComponent, ContactInfoComponent, ContactFormComponent]
 })
 export class ContactModule { }
