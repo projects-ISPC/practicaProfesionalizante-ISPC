24bbf9ff5f1fe9506d0d6a221fdfd697788027a2
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2024-06-06 10:38:38 -0300

    Se agrega activity list card

---
+++ FrontEnd 2024/App/libreria/app/src/main/java/com/proyectoispc/libreria/CardBookDashboardAdmin.java
@@ -0,0 +1,6 @@
+package com.proyectoispc.libreria;
+
+import android.app.Activity;
+
+public class CardBookDashboardAdmin extends Activity {
+}
---
+++ FrontEnd 2024/App/libreria/app/src/main/java/com/proyectoispc/libreria/ListBookDashboardAdmin.java
@@ -0,0 +1,6 @@
+package com.proyectoispc.libreria;
+
+import android.app.Activity;
+
+public class ListBookDashboardAdmin extends Activity {
+}
